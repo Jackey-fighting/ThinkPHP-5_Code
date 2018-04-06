890762cff58c2da263710f0f7af3514a8ae649aa
JackeyEting <1193767343@qq.com>
2018-04-06 08:53:20 +0800

    Update qiniu.php

--- application/extra/qiniu.php
+++ application/extra/qiniu.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'ak' => 'ToIlR4bJbmT1jzAH9mdEpP31oXEaWoYxWhFNEGhy',
-	'sk' => 'wl_0wlwvU2nFLno7jWQdAHAm1aAk9zxjL71NLIqP',
-	'bucket' => 'app-com',//这到七牛那拿这个空间名过来
+	'ak' => '********',//七牛云的ak
+	'sk' => '*******',//七牛云的sk
+	'bucket' => '****',//这到七牛那拿这个空间名过来
 	'image_url' => 'http://p6a2zcbt8.bkt.clouddn.com',//七牛的空间域名
 ];
