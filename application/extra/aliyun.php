ad3e4d076a4cf3be895c7253cb5c20bb9e39e45f
JackeyEting <1193767343@qq.com>
2018-04-06 08:54:40 +0800

    Update aliyun.php

--- application/extra/aliyun.php
+++ application/extra/aliyun.php
@@ -1,8 +1,8 @@
 <?php
 /*阿里云相关配置*/
 return [
-	'accessKeyId' => 'LTAI1qXF7qW0akBo',
-	'accessKeySecret' => 'TT7yMhV3tYfWh3X4zuKjKLOOFxJTyQ',
+	'accessKeyId' => '********',//阿里云短信的akId
+	'accessKeySecret' => '********',//阿里云短信的akSecret
 	'setSignName' => '刘湖基',
 	'setTemplateCode' => 'SMS_130390035',
 	'identify_time' => 60*60*24*7,
