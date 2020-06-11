ccf4062003039b376ef4b116a7b5fdd287396439
Jackey <1193767343@qq.com>
2020-06-11 16:07:35 +0800

    Update aliyun.php

--- application/extra/aliyun.php
+++ application/extra/aliyun.php
@@ -1,9 +1,9 @@
 <?php
 /*阿里云相关配置*/
 return [
-	'accessKeyId' => '********',//阿里云短信的akId
-	'accessKeySecret' => '********',//阿里云短信的akSecret
-	'setSignName' => '刘湖基',
-	'setTemplateCode' => 'SMS_130390035',
+	'accessKeyId' => '',//阿里云短信的akId
+	'accessKeySecret' => '',//阿里云短信的akSecret
+	'setSignName' => 'Jackey',
+	'setTemplateCode' => '',
 	'identify_time' => 60*60*24*7,
 ];
